ec7f9bcb1b34b4d25c91621df4361c3890809c54
amysir62 <amy.sir@student.mahidol.edu>
2026-03-24 15:15:42 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,34 +1,29 @@
 #include<iostream>
-#include <cstring>
-#include <cstdlib>
 using namespace std;
-
+#include <cstring>
 #include "stack.h"
-
 
 int main(int argc, char **argv){
     Stack s;
-      s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
+    s.push(5);
+    s.push(1);
+    s.push(7);
+    s.pop();
+    s.push(6);
+    s.pop();
 
- /*
-  Exercise 1*/
-    
     int i;
     for(i=1;i<argc;i++){
-       if(strcmp(argv[i],"x")==0){
-        s.pop();
-       }else{
-          s.push(atoi(argv[i]));
-       }
+        if(strcmp(argv[i], "x")==0) 
+            s.pop();
+        else 
+            s.push(atoi(argv[i]));
     }
-     
 
+    while (s.get_size()) {
+    cout << s.pop() << " ";
+   }
+    cout << endl;
 
-
-   return 0;
+    return 0;
 }
